ead26e049c2fba4da8b6a559fe1f457a2639f675
Nosa <deckengames@gmail.com>
2026-03-22 17:00:35 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import path from 'node:path';
+
+const __dirname = path.resolve();
+const app = express();
+const server = createServer();
+const bare = createBareServer('/bare/');
+
+app.use(express.static(__dirname));
+
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+server.listen({ port: process.env.PORT || 3000 });
